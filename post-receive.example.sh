5902f747b44fa7e640df193ec15342a42d3b90e9
Yair <32864499+bluepostit@users.noreply.github.com>
2021-05-14 01:08:24 +0300

    fixed example hook

--- post-receive.example.sh
+++ post-receive.example.sh
@@ -3,15 +3,15 @@
 # Copy this file, renamed as `post-receive`, into the `hooks/` directory
 # of your target git repository (deploy).
 
-GIT_WORK_TREE=/path/to/work/tree
-GIT_DIR=$GIT_WORK_TREE/.git
+TARGET_DIR=/path/to/work/tree
+GIT_DIR=$TARGET_DIR/.git
 DOCKER_IMAGE=your-user-name/docker-image-name
 
 echo ""
 echo "> Updating git working tree"
-git pull origin master
+git -C $TARGET_DIR pull origin master
 
-cd $GIT_WORK_TREE
+cd $TARGET_DIR
 echo "> Building Docker image: ${DOCKER_IMAGE}"
 sudo docker build --no-cache . -t $DOCKER_IMAGE
 
