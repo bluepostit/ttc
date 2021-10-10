5fe243990d1fe400ca5c68afd911d247024c6167
Yair <32864499+bluepostit@users.noreply.github.com>
2021-10-10 16:55:36 +0200

    interim work

--- src/plugins/session.plugin.js
+++ src/plugins/session.plugin.js
@@ -1,12 +1,19 @@
+const fs = require('fs')
+const path = require('path')
 const fp = require('fastify-plugin')
+const flash = require('fastify-flash')
 
 const plugin = async (fastify) => {
-  fastify.register(require('fastify-session'), {
-    cookieName: 'sessionId',
-    secret: process.env.SESSION_SECRET,
-    cookie: { secure: false },
+  fastify.register(require('fastify-secure-session'), {
+    cookieName: 'lwtc-session',
+    key: fs.readFileSync(path.join(__dirname, '../../secret-key')),
+    cookie: {
+      path: '/',
+      httpOnly: true
+    },
     expires: 1800000
   })
+  fastify.register(flash)
 }
 
 module.exports = fp(plugin, {
