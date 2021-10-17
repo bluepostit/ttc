fdd01af60624ef019b429a1ba0af7ceb1ae5b3f8
Yair <32864499+bluepostit@users.noreply.github.com>
2021-10-17 10:53:52 +0200

    improvements to login behavior & ux

--- src/plugins/auth.plugin.js
+++ src/plugins/auth.plugin.js
@@ -1,21 +1,26 @@
 const fp = require('fastify-plugin')
 
 const plugin = async (fastify) => {
-  const ensureSignedIn = (request, reply, next) => {
-    fastify.log.info(`Auth disabled? ${process.env.DISABLE_AUTH}`)
-    if (request.session.get('authenticated')) {
-      // Add this info to enable all views to access it as a global
-      reply.locals = {
-        signedIn: true
-      }
-      return next()
-    } else if (process.env.DISABLE_AUTH === 'true') {
-      return next()
+  fastify.addHook('preValidation', function (request, reply, done) {
+    const useAuth = process.env.DISABLE_AUTH !== 'true'
+    fastify.log.info(`Auth enabled? ${useAuth} - URL: ${request.url}`)
+    const signedIn = request.session.get('authenticated')
+    reply.locals = {
+      useAuth,
+      signedIn
     }
 
-    request.session.set('postLoginUrl', request.url)
-    request.flash('warning', 'You must sign in first')
-    reply.redirect('/auth/login')
+    return done()
+  })
+
+  const ensureSignedIn = (request, reply, next) => {
+    if (reply.locals.useAuth && !reply.locals.signedIn) {
+      request.session.set('postLoginUrl', request.url)
+      request.flash('warning', 'You must sign in first')
+      reply.redirect('/auth/login')
+    }
+
+    return next()
   }
 
   fastify.decorate('auth', {
