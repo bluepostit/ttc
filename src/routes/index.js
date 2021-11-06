19794ad1f7222920850f5d31437708615a03092b
Yair <32864499+bluepostit@users.noreply.github.com>
2021-11-06 13:33:31 +0100

    uncomment to create static data assets route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const fp = require('fastify-plugin')
 const plugin = async (fastify) => {
   // fastify.register(require('./auth.routes'))
   fastify.register(require('./base.routes'))
-  // fastify.register(require('./data-modules.routes'))
+  fastify.register(require('./data-modules.routes'))
   fastify.register(require('./schema'))
   fastify.register(require('./api-v1.routes'), { prefix: '/api/v1' })
 }
