2fcca098043dc166e3b74248051c5200b358c911
Yair <32864499+bluepostit@users.noreply.github.com>
2021-11-06 11:50:38 +0100

    minor fix: verion info file name

--- post-receive.example.sh
+++ post-receive.example.sh
@@ -6,6 +6,7 @@
 TARGET_DIR=/path/to/work/tree
 GIT_DIR=$TARGET_DIR/.git
 DOCKER_IMAGE=your-user-name/docker-image-name
+$VERSION_INFO_FILE_NAME=.version-info.yaml
 
 echo ""
 echo "> Updating git working tree"
@@ -13,7 +14,7 @@ git -C $TARGET_DIR pull origin master
 
 cd $TARGET_DIR
 echo "> Generating revision details"
-echo "version: `git rev-parse --short HEAD`\ndate: `date "+%Y-%m-%d %X %Z"`"
+echo "version: `git rev-parse --short HEAD`\ndate: `date "+%Y-%m-%d %X %Z"`" > $VERSION_INFO_FILE_NAME
 
 echo "> Building Docker image: ${DOCKER_IMAGE}"
 sudo docker build --no-cache . -t $DOCKER_IMAGE
