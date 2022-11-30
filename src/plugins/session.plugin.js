e248066587fee88be52ef27bbf32e2f609c7f226
Yair <32864499+bluepostit@users.noreply.github.com>
2022-11-30 11:28:05 +0100

    read cookie key from env variable, not file

--- src/plugins/session.plugin.js
+++ src/plugins/session.plugin.js
@@ -2,10 +2,10 @@ const fs = require('fs')
 const path = require('path')
 const fp = require('fastify-plugin')
 
-const plugin = async (fastify) => {
+const plugin = async fastify => {
   fastify.register(require('fastify-secure-session'), {
     cookieName: 'ttc-session',
-    key: fs.readFileSync(path.join(__dirname, '../../secret-key')),
+    key: Buffer.from(process.env.COOKIE_KEY, 'hex'),
     cookie: {
       path: '/',
       httpOnly: true,
